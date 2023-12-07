5b26e5726fd8f82a364ab647c25243bfdcf2d8ea
Ishana0 <ishanakalikiri01@gmailcom>
2023-12-07 16:03:11 +0530

    numeron2

--- score.js
+++ score.js
@@ -1,1 +1,7 @@
 // Iteration 8: Making scoreboard functional
+
+document.getElementById("score-board").innerText = localStorage.getItem("score");
+
+document.getElementById("play-again-button").onclick = () => {
+    window.location.href = "./game.html";
+};
